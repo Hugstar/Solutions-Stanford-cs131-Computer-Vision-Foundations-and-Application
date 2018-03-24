b17064a4666575b1d35baf149a179277c326e305
Matan <matan@camerai.co>
2018-03-24 14:39:00 +0300

    HW1 Done!

--- hw1_release/filters.py
+++ hw1_release/filters.py
@@ -146,7 +146,10 @@ def cross_correlation(f, g):
 
     out = None
     ### YOUR CODE HERE
-    pass
+    print ('f shape {} g shape {}'.format(f.shape, g.shape))
+    g = np.flip(np.flip(g,axis=0), axis=1)
+    # Had to change g dimension to be an odd number. even filters are not supported.
+    out = conv_fast(f,g[0:-1])
     ### END YOUR CODE
 
     return out
@@ -166,7 +169,10 @@ def zero_mean_cross_correlation(f, g):
 
     out = None
     ### YOUR CODE HERE
-    pass
+    g_mean = np.sum(g)/np.size(g)
+    g = g - g_mean
+    g = np.flip(np.flip(g, axis=0), axis=1)
+    out =  conv_fast(f, g[0:-1])
     ### END YOUR CODE
 
     return out
@@ -186,8 +192,32 @@ def normalized_cross_correlation(f, g):
     """
 
     out = None
+
     ### YOUR CODE HERE
-    pass
+    # We don't support even kernel dimensions
+    if g.shape[0] % 2 == 0:
+        g = g[0:-1]
+    if g.shape[1] % 2 == 0:
+        g = g[:,0:-1]
+    assert g.shape[0] % 2 == 1 and g.shape[1] % 2 == 1, "Even dimensions for filters is not allowed!"
+
+    Hk, Wk = g.shape
+    Hi, Wi = f.shape
+    out = np.zeros((Hi, Wi))
+
+    normalized_filter = (g - np.mean(g)) / np.std(g)
+    assert np.mean(normalized_filter) < 1e-5, "g mean is {}, should be 0".format(np.mean(g))
+    assert np.abs(np.std(normalized_filter) - 1) < 1e-5, "g std is {}, should be 1".format(np.std(g))
+
+    delta_h = int((Hk - 1) / 2)
+    delta_w = int((Wk - 1) / 2)
+    for image_h in range(delta_h, Hi - delta_h):
+        for image_w in range(delta_w, Wi - delta_w):
+            image_patch =f[image_h - delta_h:image_h + delta_h + 1, image_w - delta_w:image_w + delta_w + 1]
+            normalized_image_patch = (image_patch - np.mean(image_patch)) / np.std(image_patch)
+            assert np.mean(normalized_image_patch) < 1e-5, "g mean is {}, should be 0".format(np.mean(g))
+            assert np.abs(np.std(normalized_image_patch) - 1) < 1e-5, "g std is {}, should be 1".format(np.std(g))
+            out[image_h][image_w] = np.sum(normalized_filter * normalized_image_patch)
     ### END YOUR CODE
 
     return out
